bebcb6bb1c4e6df43fc5301a130b32a2d95f2a96
Diego Higuita <diegohiguita1993@hotmail.com>
2024-09-12 21:10:47 -0500

    Se crea contolador,servicio y validacion para Remitente

--- src/main/java/com/example/BODEGASTCCAPI/helpers/mensajes/Mensaje.java
+++ src/main/java/com/example/BODEGASTCCAPI/helpers/mensajes/Mensaje.java
@@ -4,7 +4,10 @@ public enum Mensaje {
 
     PESO_NEGATIVO("El peso no puede ser negativo"),
     VOLUMEN_NEGATIVO("eL volumen no puede ser negativo"),
-    FECHA_INVALIDA("Revisa la fecha ingresada")
+    FECHA_INVALIDA("Revisa la fecha ingresada"),
+    DEPARTAMENTO_INVALIDO("Verifica el departamento ingresado"),
+    METODO_PAGO_INVALIDO("Metodo de pago no valido"),
+    NOMBRE_INVALIDO("El nombre que ingresaste es invalido, verificalo")
 
     ;
 
